7f1450813ff33dffb998d14206cbfe08a1e61e53
redaxmedia <info@redaxmedia.com>
2016-11-21 22:47:13 +0100

    Update config script

--- config.sh
+++ config.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-replace 'UA-00000000-0' 'UA-16122280-4' -- build/modules/Analytics/assets/scripts/init.js
-replace 'auto' 'demo.redaxscript.com' -- build/modules/Analytics/assets/scripts/init.js
+replace 'UA-00000000-0' 'UA-16122280-1' -- build/modules/Analytics/assets/scripts/init.js
+replace 'auto' 'redaxscript.com' -- build/modules/Analytics/assets/scripts/init.js
+echo sitemap: https://redaxscript.com/sitemap_xml > build/robots.txt
 
 cd build
 php console.php config parse --db-url $DB_URL
