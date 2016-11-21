afa2c5e3d85e79fbb73d9b5ad2d5cae74bf9d3ca
redaxmedia <info@redaxmedia.com>
2016-11-21 23:49:47 +0100

    Create files directory

--- config.sh
+++ config.sh
@@ -3,9 +3,9 @@
 replace 'UA-00000000-0' 'UA-16122280-1' -- build/modules/Analytics/assets/scripts/init.js
 replace 'auto' 'redaxscript.com' -- build/modules/Analytics/assets/scripts/init.js
 echo sitemap: https://redaxscript.com/sitemap_xml > build/robots.txt
-ln -s ../redaxscript-files build/files
 
 cd build
 php console.php config parse --db-url $DB_URL
 php console.php status database
 rm console.php
+mkdir files
