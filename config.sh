be8dcf51f674c92674ba34b4b2378606482d186e
redaxmedia <info@redaxmedia.com>
2016-11-21 23:01:50 +0100

    Add check for connection

--- config.sh
+++ config.sh
@@ -6,4 +6,5 @@ echo sitemap: https://redaxscript.com/sitemap_xml > build/robots.txt
 
 cd build
 php console.php config parse --db-url $DB_URL
+php console.php status database
 rm console.php
--- gruntfile.js
+++ gruntfile.js
@@ -21,7 +21,7 @@ module.exports = function (grunt)
 		},
 		copy:
 		{
-			distDemo:
+			distWebsite:
 			{
 				src:
 				[
