3108e81f78f5f6e60cae628568d3997bd2860ee1
redaxmedia <info@redaxmedia.com>
2016-11-25 00:06:57 +0100

    Extend the analytics selector with label

--- config.sh
+++ config.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+replace 'a.rs-js-track-click, button.rs-js-track-click' 'a.rs-js-track-click, button.rs-js-track-click, label.rs-js-track-click' -- build/modules/Analytics/assets/scripts/init.js
 replace 'UA-00000000-0' 'UA-16122280-1' -- build/modules/Analytics/assets/scripts/init.js
 replace 'auto' 'redaxscript.com' -- build/modules/Analytics/assets/scripts/init.js
 replace '0000000000000000000000' 'RHRZD6JfTlizKGb2EZMEFA' -- build/modules/Experiments/Config.php
