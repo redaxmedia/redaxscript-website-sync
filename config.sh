b7cb89c601b28b00cf959f21b7767dff746245c0
redaxmedia <info@redaxmedia.com>
2016-12-09 23:37:36 +0100

    Remove the Experiments module

--- config.sh
+++ config.sh
@@ -3,7 +3,6 @@
 replace 'a.rs-js-track-click, button.rs-js-track-click' 'a.rs-js-track-click, button.rs-js-track-click, label.rs-js-track-click' -- build/modules/Analytics/assets/scripts/init.js
 replace 'UA-00000000-0' 'UA-16122280-1' -- build/modules/Analytics/assets/scripts/init.js
 replace 'auto' 'redaxscript.com' -- build/modules/Analytics/assets/scripts/init.js
-replace '0000000000000000000000' 'RHRZD6JfTlizKGb2EZMEFA' -- build/modules/Experiments/Config.php
 echo sitemap: https://redaxscript.com/sitemap_xml > build/robots.txt
 
 cd build
--- gruntfile.js
+++ gruntfile.js
@@ -34,7 +34,6 @@ module.exports = function (grunt)
 					'libraries/**',
 					'modules/Analytics/**',
 					'modules/DirectoryLister/**',
-					'modules/Experiments/**',
 					'modules/PageCache/**',
 					'modules/SitemapXml/**',
 					'modules/SyntaxHighlighter/**',
