a1746099db2c35fcb82c76de0bb64d1aa9ae0938
redaxmedia <info@redaxmedia.com>
2018-10-06 23:25:32 +0200

    Use latest sitemap format

--- tasks/shell.js
+++ tasks/shell.js
@@ -22,7 +22,7 @@ module.exports = () =>
 				'replace \'a.rs-js-track-click, button.rs-js-track-click\' \'a.rs-js-track-click, button.rs-js-track-click, label.rs-js-track-click\' -- build/modules/Analytics/assets/scripts/init.js',
 				'replace \'UA-00000000-0\' \'UA-16122280-1\' -- build/modules/Analytics/assets/scripts/init.js',
 				'replace \'auto\' \'redaxscript.com\' -- build/modules/Analytics/assets/scripts/init.js',
-				'echo sitemap: https://redaxscript.com/sitemap_xml > build/robots.txt'
+				'echo sitemap: https://redaxscript.com/module/sitemap-xml > build/robots.txt'
 			]
 			.join('&&')
 		},
